4236f69045030ff6fd1efd5acb5cd48aeae31c3b
George Babichev <gbabichev1@gmail.com>
2026-01-09 18:10:41 -0500

    Initial Commit

---
+++ Security Cameras/Security_CamerasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Security_CamerasApp.swift
+//  Security Cameras
+//
+//  Created by George Babichev on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Security_CamerasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
